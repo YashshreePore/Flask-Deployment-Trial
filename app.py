540e3b8ba01166965040f7c6646658c9d278b812
Yashshree Pore <yashshreepore@gmail.com>
2025-11-16 20:03:43 +0530

    Updated message for second deployment trial

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     # इथून यूजर्सला एक साधे वेब पेज दिसेल
-    return '<h1>Hello, Heroku! This is our deployment trial.</h1>'
+    return '<h1>Hello, Render! This is our SECOND deployment and it works!</h1>'
 
 if __name__ == '__main__':
     # पोर्ट ॲडजस्टमेंट Heroku स्वतः करते, पण लोकल टेस्टसाठी हे चांगले आहे.
